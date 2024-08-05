5d1eebe03156dfa6d353744a544b3d9da561f80d
Robert.Roberts <robmroberts93@gmail.com>
2024-08-05 12:54:15 -0400

    Hand off push

--- java/src/main/java/com/techelevator/service/ComicService.java
+++ java/src/main/java/com/techelevator/service/ComicService.java
@@ -1,4 +1,27 @@
 package com.techelevator.service;
 
+
+import com.techelevator.dao.ComicDao;
+import com.techelevator.model.Comic;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ComicService {
+    private final ComicDao comicDao;
+
+    public ComicService(ComicDao comicDao) {
+        this.comicDao = comicDao;
+    }
+
+    public Comic getComicById(String comicId){
+        return comicDao.getComicById(comicId);
+    }
+    public List<Comic> getComics(){
+        return comicDao.getComics();
+    }
+    public Comic saveComic(Comic comic){
+        return comicDao.saveComic(comic);
+    }
 }
