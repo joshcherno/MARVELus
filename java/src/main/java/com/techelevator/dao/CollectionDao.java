109b6eea1eac8980560868c14806b65e904a4c0e
Robert.Roberts <robmroberts93@gmail.com>
2024-08-05 12:20:35 -0400

    safety commit

---
+++ java/src/main/java/com/techelevator/dao/CollectionDao.java
@@ -0,0 +1,11 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Collection;
+import com.techelevator.model.User;
+
+public interface CollectionDao {
+
+    Collection getCollectionByUserId(userId);
+
+
+}
---
+++ java/src/main/java/com/techelevator/model/Comic.java
@@ -0,0 +1,62 @@
+package com.techelevator.model;
+
+import java.time.LocalDate;
+
+public class Comic {
+
+    private String comicId;
+    private String title;
+    private String author;
+    private String description;
+    private LocalDate releaseDate;
+    private String coverArt;
+
+
+    public String getComicId() {
+        return comicId;
+    }
+
+    public void setComicId(String comicId) {
+        this.comicId = comicId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getCoverArt() {
+        return coverArt;
+    }
+
+    public void setCoverArt(String coverArt) {
+        this.coverArt = coverArt;
+    }
+}
