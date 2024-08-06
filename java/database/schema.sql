bc813c17fef994c8cee57e150bee0bee87b6b8ad
Mehmet Gun <mehmath89@gmail.com>
2024-08-06 12:55:49 -0400

    Updated

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE comic (
     comic_author varchar(50),
     description varchar(250),
     release_date date,
-    cover_url varchar(100)
+    cover_url varchar(100),
     CONSTRAINT PK_comic_id PRIMARY KEY (comic_id)
 );
 --CREATE TABLE collection (
@@ -23,5 +23,15 @@ CREATE TABLE comic (
 --    collection_name varchar(50),
 --    collection_description varchar(250)
 --    ) TODO collection and comicCollection table
+
+CREATE TABLE collection_comics (
+    collection_comic_id varchar(50) NOT NULL,
+    collection_id int NOT NULL,
+    comic_id int NOT NULL,
+    added_date date NOT NULL,
+    CONSTRAINT PK_comic PRIMARY KEY (comic_id)
+);
+
+
 
 COMMIT TRANSACTION;
--- java/src/main/java/com/techelevator/service/ApiService.java
+++ java/src/main/java/com/techelevator/service/ApiService.java
@@ -6,52 +6,72 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import java.time.Instant;
 
-@Service
+
+// @Service
 public class ApiService {
 
-    @Value("${marvel.api.base-url}")
-    private String baseUrl;
+//    @Value("${marvel.api.base-url}")
+//    private String baseUrl;
+//
+//    @Value("${marvel.api.public-key}")
+//    private String publicKey;
+//
+//    @Value("${marvel.api.private-key}")
+//    private String privateKey;
+//
+//    private final RestTemplate restTemplate;
+//
+//    public ApiService(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
+//    public String getComicById(Long comicId) {
+//        // Generate timestamp
+//        String ts = String.valueOf(Instant.now().getEpochSecond());
+//
+//        // Generate hash
+//        String hash = DigestUtils.md5Hex(ts + privateKey + publicKey);
+//
+//
+//        String url = UriComponentsBuilder.fromHttpUrl(baseUrl + "/comics/{id}")
+//                .queryParam("ts", ts)
+//                .queryParam("apikey", publicKey)
+//                .queryParam("hash", hash)
+//                .buildAndExpand(comicId)
+//                .toUriString();
+//
+//        // Make API call
+//        return restTemplate.getForObject(url, String.class);
+//    }
+//    public String getComics(){
+//        String ts = String.valueOf(Instant.now().getEpochSecond());
+//        String hash = DigestUtils.md5Hex(ts + privateKey + publicKey);
+//        String url = UriComponentsBuilder.fromHttpUrl(baseUrl + "/comics/{id}")
+//                .queryParam("ts", ts)
+//                .queryParam("apikey", publicKey)
+//                .queryParam("hash", hash)
+//                .toUriString();
+//        return restTemplate.getForObject(url, String.class);
+//    }
 
-    @Value("${marvel.api.public-key}")
-    private String publicKey;
+    public static final String MARVEL_URL_API = "https://gateway.marvel.com/v1/public";
+    public static final String MARVEL_URI_COMICS = "/comics";
+    public static final String MARVEL_URI_CHARACTERS = "/characters";
+    public static final String MARVEL_URI_CREATORS = "/creators";
+    public static final String MARVEL_URI_EVENTS = "/events";
+    public static final String MARVEL_URI_SERIES = "/series";
+    public static final String MARVEL_PARAM_TIMESTAMP = "1";
+    public static final String MARVEL_PARAM_APIKEY = "25b2c83d1b49f1d2ab163802b4a2cda6";
+    public static final String MARVEL_PARAM_HASH = "bd3b0817e4b8aee1b1e8f226a56de70b";
+    public static final String MARVEL_PARAM_LIMIT = "limit";
 
-    @Value("${marvel.api.private-key}")
-    private String privateKey;
+    public ApiService(){
 
-    private final RestTemplate restTemplate;
-
-    public ApiService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-    public String getComicById(Long comicId) {
-        // Generate timestamp
-        String ts = String.valueOf(Instant.now().getEpochSecond());
-
-        // Generate hash
-        String hash = DigestUtils.md5Hex(ts + privateKey + publicKey);
-
-
-        String url = UriComponentsBuilder.fromHttpUrl(baseUrl + "/comics/{id}")
-                .queryParam("ts", ts)
-                .queryParam("apikey", publicKey)
-                .queryParam("hash", hash)
-                .buildAndExpand(comicId)
-                .toUriString();
-
-        // Make API call
-        return restTemplate.getForObject(url, String.class);
-    }
-    public String getComics(){
-        String ts = String.valueOf(Instant.now().getEpochSecond());
-        String hash = DigestUtils.md5Hex(ts + privateKey + publicKey);
-        String url = UriComponentsBuilder.fromHttpUrl(baseUrl + "/comics/{id}")
-                .queryParam("ts", ts)
-                .queryParam("apikey", publicKey)
-                .queryParam("hash", hash)
-                .toUriString();
-        return restTemplate.getForObject(url, String.class);
     }
 
 
