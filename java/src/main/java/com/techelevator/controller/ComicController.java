141b51f32d15e230035af5504a2870477a067c9c
Robert.Roberts <robmroberts93@gmail.com>
2024-08-07 15:21:38 -0400

    Restored mild functionality to the backend - Rob

--- java/src/main/java/com/techelevator/controller/ComicController.java
+++ java/src/main/java/com/techelevator/controller/ComicController.java
@@ -1,8 +1,10 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.ComicDao;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.techelevator.model.Comic;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @CrossOrigin
@@ -13,6 +15,24 @@ public class ComicController {
 
     public ComicController(ComicDao comicDao){
         this.comicDao = comicDao;
+    }
+
+    @RequestMapping(path = "/comic/{comicId}", method = RequestMethod.GET)
+    public Comic getComicById(@PathVariable int comicId, Principal principal) {
+        return comicDao.getComicById(comicId);
+    }
+    @RequestMapping(path = "/comic/{comicTitle}", method = RequestMethod.GET)
+    public Comic getComicByTitle(@PathVariable String title, Principal principal) {
+        return comicDao.getComicByTitle(title);
+    }
+    @RequestMapping(path = "/comic/{author}", method = RequestMethod.GET)
+    public Comic getComicByAuthor(@PathVariable String author){
+        return comicDao.getComicByAuthor(author);
+    }
+    @RequestMapping(path = "/comic", method = RequestMethod.PUT)
+    public Comic saveComic(@RequestBody Comic comic){
+        return comicDao.saveComic(comic);
+
     }
 
 
